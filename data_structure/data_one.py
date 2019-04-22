a98a5723c1c84b88da7cbd30cf8c12c549def5a4
wjm <1563374406@qq.com>
2019-04-22 11:47:06 +0800

    python cookbook

---
+++ data_structure/data_one.py
@@ -0,0 +1,24 @@
+# from audioop import avg
+#
+#
+# def drop_first_last(grades):
+#     first, *middle, last = grades
+#     return avg(middle)
+
+# records = [('foo', 1, 2), ('bar', 'hello'), ('foo', 3, 4), ]
+#
+#
+# def do_foo(x, y):
+#     print('foo', x, y)
+#
+#
+# def do_bar(s):
+#     print('bar', s)
+#
+#
+# for tag, *args in records:
+#     if tag == 'foo':
+#         do_foo(*args)
+#     elif tag == 'bar':
+#         do_bar(*args)
+
